9c1e7a452ea52471099cc7efedb65ecdd865d533
Nicolas Guérin <nicolas.guerin.pro@laposte.net>
2024-05-20 15:40:30 +0200

    Setup for var, values, link

--- src/app/components/feature/loadout/loadout.component.ts
+++ src/app/components/feature/loadout/loadout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GameState } from 'src/app/models/game-state.model';
+import { GameStateService } from 'src/app/shared/game-state.service';
 
 @Component({
   selector: 'app-loadout',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./loadout.component.scss']
 })
 export class LoadoutComponent {
+
+  gameState!: GameState;
+
+  constructor(public gss: GameStateService) {}
+
+  ngOnInit() {
+    this.gss.getGameState().subscribe(gs => {
+      this.gameState = gs;
+    });
+  }
+
+  onSelectedCultReceive(englishName: string): void {
+    this.gss.setCult(englishName);
+  }
+
+  onSelectedLeaderReceive(englishName: string): void {
+    this.gss.setLeader(englishName);
+  }
 
 }
